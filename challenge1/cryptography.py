07010a48e66147ed161ac68b079f70c935a0db48
Cullen Middleton <culmid@gmail.com>
2022-05-16 16:56:59 +0200

    init structure

---
+++ challenge1/cryptography.py
@@ -0,0 +1,4 @@
+def rot13(plaintext: str) -> str:
+    """Returns the ciphertext resulting from encrypting `plaintext` using the rot13 algorithm"""
+    # ... do the encryption...
+    print(plaintext)
---
+++ challenge1/tests.py
@@ -0,0 +1,10 @@
+import unittest
+
+
+class CryptoTests(unittest.TestCase):
+    def test_single_lower_case(self):
+        self.assertTrue(True)
+
+
+if __name__ == "__main__":
+    unittest.main()
