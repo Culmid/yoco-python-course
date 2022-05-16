a6715a45041c470ad38ca7c2971d3eaed8bc9d17
Cullen Middleton <culmid@gmail.com>
2022-05-16 17:22:58 +0200

    single lower case wrap

--- challenge1/cryptography.py
+++ challenge1/cryptography.py
@@ -1,4 +1,4 @@
 def rot13(plaintext: str) -> str:
     """Returns the ciphertext resulting from encrypting `plaintext` using the rot13 algorithm"""
     # ... do the encryption...
-    return chr(ord(plaintext) + 13)
+    return chr((ord(plaintext) - 97 + 13) % 26 + 97)
--- challenge1/tests.py
+++ challenge1/tests.py
@@ -8,6 +8,11 @@ class CryptoTests(unittest.TestCase):
         self.assertEqual(rot13("b"), "o")
         self.assertEqual(rot13("c"), "p")
 
+    def test_single_lower_case_wrap(self):
+        self.assertEqual(rot13("x"), "k")
+        self.assertEqual(rot13("y"), "l")
+        self.assertEqual(rot13("z"), "m")
+
 
 if __name__ == "__main__":
     unittest.main()
