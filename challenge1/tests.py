88e12997af700c5a6bcbe4d9788a2b5ca26589f2
Cullen Middleton <culmid@gmail.com>
2022-05-16 17:07:34 +0200

    single lower case

--- challenge1/tests.py
+++ challenge1/tests.py
@@ -1,9 +1,12 @@
 import unittest
+from cryptography import rot13
 
 
 class CryptoTests(unittest.TestCase):
     def test_single_lower_case(self):
-        self.assertTrue(True)
+        self.assertEqual(rot13("a"), "n")
+        self.assertEqual(rot13("b"), "o")
+        self.assertEqual(rot13("c"), "p")
 
 
 if __name__ == "__main__":
