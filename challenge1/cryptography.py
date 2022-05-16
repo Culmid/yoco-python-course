387a78a4b041ede88a4af2ca2538bb65e1d99e56
Cullen Middleton <culmid@gmail.com>
2022-05-16 17:30:08 +0200

    single upper case

--- challenge1/cryptography.py
+++ challenge1/cryptography.py
@@ -1,4 +1,9 @@
 def rot13(plaintext: str) -> str:
     """Returns the ciphertext resulting from encrypting `plaintext` using the rot13 algorithm"""
     # ... do the encryption...
-    return chr((ord(plaintext) - 97 + 13) % 26 + 97)
+    return translate(plaintext)
+
+
+def translate(plaintext: str) -> str:
+    ascii_start = 65 if plaintext.isupper() else 97
+    return chr((ord(plaintext) - ascii_start + 13) % 26 + ascii_start)
--- challenge1/tests.py
+++ challenge1/tests.py
@@ -13,6 +13,16 @@ class CryptoTests(unittest.TestCase):
         self.assertEqual(rot13("y"), "l")
         self.assertEqual(rot13("z"), "m")
 
+    def test_single_upper_case(self):
+        self.assertEqual(rot13("D"), "Q")
+        self.assertEqual(rot13("E"), "R")
+        self.assertEqual(rot13("F"), "S")
+
+    def test_single_upper_case_wrap(self):
+        self.assertEqual(rot13("U"), "H")
+        self.assertEqual(rot13("V"), "I")
+        self.assertEqual(rot13("W"), "J")
+
 
 if __name__ == "__main__":
     unittest.main()
