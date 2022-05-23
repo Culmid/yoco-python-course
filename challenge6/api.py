0b7d6d66e8ac53ddb0585ff789f3304bc0f0e058
Cullen Middleton <culmid@gmail.com>
2022-05-23 09:59:27 +0200

    Basic Endpoint

--- challenge6/api.py
+++ challenge6/api.py
@@ -1,11 +1,12 @@
-from flask import Flask
+from flask import Flask, request
 from dotenv import load_dotenv
+from cryptography import rot13
 
 load_dotenv()
 
 app = Flask(__name__)
 
 
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+@app.route("/api/rot13", methods=["GET"])
+def rot13_translation():
+    return {"text": rot13(request.json["text"])}
